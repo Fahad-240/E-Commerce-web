df362c76b25e054cce695129120ffb8a6c2a9fd2
Fahad Ahmed <fahadahmed42003@gmail.com>
2026-03-31 01:42:39 +0500

    change code db.mjs

--- db.mjs
+++ db.mjs
@@ -1,15 +1,13 @@
 import 'dotenv/config'
-import { Pool } from "pg";
+import pkg from "pg";
 
-export const db = new Pool({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: 5432,
-    database: process.env.PGDATABASE,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
+const { Pool } = pkg;
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export default db;
